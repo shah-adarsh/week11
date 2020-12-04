0a1ab3b6e3ac7612161490bbf9e15d73a5b371e0
shah-adarsh <72192419+shah-adarsh@users.noreply.github.com>
2020-12-04 18:22:25 +0000

    Add files via upload

--- users.php
+++ users.php
@@ -1,6 +1,8 @@
 <?php
 include 'connect.php';
 
+
+
 if(!isset(($_POST['search']))||($_POST['search'] === "")){
     $sql = "SELECT firstname, lastname, email from livesearch ORDER BY lastname ASC";
 }
@@ -9,6 +11,14 @@ else{
 $sql = "select firstname, lastname, email from livesearch where firstname like '%$search%' or lastname like '%$search%' ORDER BY lastname ASC";
 
 }
+$query = $conn->query($sql);
+$users = array();
+
+while ($row = $query->fetch_arrray()){
+	array_push($users,$row);
+	}
+$conn->close();
+
 header("Content-type: application/json");
 echo json_encode($users);
 die();
